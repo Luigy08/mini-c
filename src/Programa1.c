747406691cc134d14043057b07304b7ffa6bba60
alejandro-g <diego.galdamez@unitec.edu>
2020-06-09 19:17:33 -0600

    Ya hace condicional de IF dentro del cuerpo. Toca arreglar codigo intermedio

--- src/Programa1.c
+++ src/Programa1.c
@@ -1,16 +1,17 @@
 int variable, variable2;
 char char1, char2; 
 int mifuncion(int a, char b);  
+int x; 
 
 void main() {
 int edad,resultado; 
 char genero; 
 edad = 54 + 9; 
 resultado = (edad + 10) * 60;  
+
+    if(x < 1 AND x > 2){
+        x = 3; 
+    }
+
 return;
 }
-
-int mifuncion(int a, char b){   
-}
- 
-
