a1243f8aecd1215eb716444f6fd195ad4e745077
alejandro-g <diego.galdamez@unitec.edu>
2020-06-10 20:25:43 -0600

    cometic changes al arbol

--- src/Programa1.c
+++ src/Programa1.c
@@ -10,7 +10,7 @@ edad = 54 + 9;
 resultado = (edad + 10) * 60;  
 
     if(x == 1 || x != 2){
-        for(i = 0; i <= 10; i++){
+        for(i = 0; i <= 10; i--){
             char1 = 'a'; 
         }
     }
