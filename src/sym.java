e6a135439cf49c4faf1e108e06df56b2e4ae1c40
Luigy08 <luigy9696@gmail.com>
2020-06-27 19:48:39 -0600

    printf y scanf

--- src/sym.java
+++ src/sym.java
@@ -6,69 +6,72 @@
 /** CUP generated class containing symbol constants. */
 public class sym {
   /* terminals */
-  public static final int CHARESPECIAL = 47;
+  public static final int CHARESPECIAL = 49;
   public static final int FUNCTION = 4;
-  public static final int OPSUM = 34;
-  public static final int CHAR = 19;
-  public static final int DECISION = 55;
-  public static final int QUESTIONMARK = 54;
-  public static final int OPDECLARACION = 27;
-  public static final int DECREMENTADOR = 57;
-  public static final int XOR = 29;
-  public static final int INTEGER = 20;
-  public static final int REPEAT = 39;
-  public static final int INT = 14;
-  public static final int PUNTOCOMA = 46;
-  public static final int CURLYDER = 53;
-  public static final int FOR = 38;
-  public static final int OPBOOL = 25;
+  public static final int OPSUM = 36;
+  public static final int SCANF = 12;
+  public static final int CHAR = 21;
+  public static final int DECISION = 58;
+  public static final int QUESTIONMARK = 57;
+  public static final int OPDECLARACION = 29;
+  public static final int DECREMENTADOR = 60;
+  public static final int XOR = 31;
+  public static final int INTEGER = 22;
+  public static final int REPEAT = 41;
+  public static final int INT = 16;
+  public static final int PUNTOCOMA = 48;
+  public static final int ASTERISCO = 50;
+  public static final int CURLYDER = 56;
+  public static final int FOR = 40;
+  public static final int OPBOOL = 27;
   public static final int WRITE = 7;
-  public static final int IDENTIFICADOR = 58;
-  public static final int NOT = 31;
-  public static final int AND = 28;
-  public static final int RECORD = 22;
-  public static final int TYPE = 21;
-  public static final int OR = 30;
-  public static final int BOOL = 16;
-  public static final int STRING1 = 23;
+  public static final int IDENTIFICADOR = 61;
+  public static final int NOT = 33;
+  public static final int AND = 30;
+  public static final int RECORD = 24;
+  public static final int TYPE = 23;
+  public static final int OR = 32;
+  public static final int BOOL = 18;
+  public static final int STRING1 = 25;
   public static final int BEGIN = 2;
-  public static final int UNTIL = 42;
-  public static final int OPREL = 33;
-  public static final int NUM = 59;
+  public static final int UNTIL = 44;
+  public static final int OPREL = 35;
+  public static final int NUM = 62;
   public static final int MAIN = 10;
-  public static final int IF = 11;
-  public static final int CONSTSTRING = 62;
-  public static final int DOT = 44;
-  public static final int PARIZQ = 49;
+  public static final int IF = 13;
+  public static final int CONSTSTRING = 65;
+  public static final int DOT = 46;
+  public static final int PARIZQ = 52;
   public static final int EOF = 0;
-  public static final int BOOLEAN = 24;
+  public static final int BOOLEAN = 26;
   public static final int RETURN = 9;
-  public static final int INCREMENTADOR = 56;
-  public static final int EQUAL = 32;
-  public static final int BRACKETIZQ = 50;
+  public static final int INCREMENTADOR = 59;
+  public static final int EQUAL = 34;
+  public static final int BRACKETIZQ = 53;
   public static final int error = 1;
   public static final int PROGRAM = 6;
-  public static final int COMA = 45;
-  public static final int VOID = 15;
-  public static final int NUMREAL = 60;
-  public static final int ASIGNACION = 26;
-  public static final int ELSE = 12;
+  public static final int COMA = 47;
+  public static final int VOID = 17;
+  public static final int PRINTF = 11;
+  public static final int NUMREAL = 63;
+  public static final int ASIGNACION = 28;
+  public static final int ELSE = 14;
   public static final int READ = 8;
-  public static final int TO = 41;
-  public static final int WHILE = 37;
-  public static final int FLOAT = 18;
-  public static final int OPCONDICIONALES = 36;
-  public static final int THEN = 13;
-  public static final int CURLYIZQ = 52;
+  public static final int TO = 43;
+  public static final int WHILE = 39;
+  public static final int FLOAT = 20;
+  public static final int OPCONDICIONALES = 38;
+  public static final int THEN = 15;
+  public static final int CURLYIZQ = 55;
   public static final int END = 3;
-  public static final int CONSTCHAR = 61;
-  public static final int DOWNTO = 43;
-  public static final int PARDER = 48;
-  public static final int BRACKETDER = 51;
-  public static final int OPMULT = 35;
-  public static final int VAR = 17;
+  public static final int CONSTCHAR = 64;
+  public static final int DOWNTO = 45;
+  public static final int PARDER = 51;
+  public static final int BRACKETDER = 54;
+  public static final int OPMULT = 37;
+  public static final int VAR = 19;
   public static final int PROCEDURE = 5;
-  public static final int DO = 40;
+  public static final int DO = 42;
   public static final String[] terminalNames = new String[] {
   "EOF",
   "error",
@@ -81,6 +84,8 @@ public class sym {
   "READ",
   "RETURN",
   "MAIN",
+  "PRINTF",
+  "SCANF",
   "IF",
   "ELSE",
   "THEN",
@@ -118,6 +123,7 @@ public class sym {
   "COMA",
   "PUNTOCOMA",
   "CHARESPECIAL",
+  "ASTERISCO",
   "PARDER",
   "PARIZQ",
   "BRACKETIZQ",
---
+++ src/test1.c
@@ -0,0 +1,13 @@
+int variable, variable2;
+char char1, char2;
+int mifuncion(int a, char b);
+
+void main() {
+  /*hola mundo*/
+int edad,resultado;
+edad=28 + 2;
+
+printf("sdfsdf %c", edad);
+
+return 0;
+}
